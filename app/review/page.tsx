14a121bc9ac7098da4e9f728c7c30c0d60f86a8b
Vishwam-work <vishuwork247@gmail.com>
2025-11-02 17:21:06 +0530

    [FIX] all: change the url local to live

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -33,7 +33,7 @@ export default function ProfileReview() {
   const [jobCategories, setJobCategories] = useState([]);
 
     useEffect(() => {
-      fetch("http://127.0.0.1:8010/master/api/jobs_title/")
+      fetch("https://jobseeker-backend-jy1y.onrender.com/master/api/jobs_title/")
         .then((res) => res.json())
         .then((data) => {
           setJobTitles(data);
@@ -42,7 +42,7 @@ export default function ProfileReview() {
     }, []);
 
     useEffect(() => {
-      fetch("http://127.0.0.1:8010/master/api/jobs_category/")
+      fetch("https://jobseeker-backend-jy1y.onrender.com/master/api/jobs_category/")
         .then((res) => res.json())
         .then((data) => {
           setJobCategories(data);
@@ -60,7 +60,7 @@ export default function ProfileReview() {
   };
   useEffect(() => {
     const loadProfile = async () => {
-      const res = await fetch("http://127.0.0.1:8010/api/profile/", {
+      const res = await fetch("https://jobseeker-backend-jy1y.onrender.com/api/profile/", {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${localStorage.getItem("auth_token")}`,
